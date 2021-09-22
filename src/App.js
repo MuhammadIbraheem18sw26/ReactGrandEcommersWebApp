7ac3652686ed4d354ec4411b4f18b04d7d15e6eb
Muhammad Ibraheem <ibrahimjhullan7@gmail.com>
2021-09-23 00:00:27 +0500

    Router used

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import './App.css';
-import Homepage from './pages/homepage/Homepage.comnent.jsx';
+import Homepage from './pages/homepage/Homepage.comnent';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats page</h1>
+  </div>
+);
 
 function App() {
   return (
-    <Homepage />
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route path='/hats' component={HatsPage} />
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
